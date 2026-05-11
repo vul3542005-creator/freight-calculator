1281b48045adb1865f07bc2e91e6bd55489e0672
vul3542005-creator <vul3542005@gmail.com>
2026-05-11 09:05:53 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ async function loadEngineData() {
   show(DOM.loadingOverlay);
   try {
     const [sdRes, mrRes] = await Promise.all([
-      fetch('special_delivery_v2.json'),
+      fetch('special_delivery_v3-1.json'),
       fetch('manual_rules.json'),
     ]);
     for (const [name, res] of [
